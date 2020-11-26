728de1f1b1eb7200739a67b2bee6dd9ea9a9d6b3
Santiago Ramon <santiagomramon@gmail.com>
2020-11-26 19:01:56 -0300

    Este es el primer commit

---
+++ typescript/index.ts
@@ -0,0 +1,14 @@
+console.log('Hello Typescript')
+
+
+let a = 1
+let b = 2
+
+function sumar(a: number,b: number){
+
+    return a+b
+}
+
+let resultado = sumar(a,b);
+
+console.log(`El resultado de la suma es ${resultado}`)
